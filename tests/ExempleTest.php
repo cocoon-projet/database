466bc0ddcebdfe43c5019d4d89f876a9118b7eac
cocoon-projet <contact@cocoon-projet.fr>
2025-02-23 17:30:06 +0100

    init testing

---
+++ tests/ExempleTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExempleTest extends TestCase
+ {
+    public function testTrueAssertsToTrue()
+    {
+        $this->assertTrue(true);
+    }
+    
+ }
